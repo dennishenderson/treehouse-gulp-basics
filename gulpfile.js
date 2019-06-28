b3d149baade12a8315eb8f3e6c4d0974fec53602
Dennis Henderson <dennismhenderson@gmail.com>
2019-06-28 04:24:53 -0400

    Setup uglify and rename gulp script

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,26 @@
-"use strict";
+'use strict';
 
 var gulp = require('gulp'),
-  concat = require('gulp-concat');
+  concat = require('gulp-concat'),
+  uglify = require('gulp-uglify'),
+  rename = require('gulp-rename');
 
 gulp.task("concatScripts", function() {
-    gulp.src([
-        'js/jquery.js',
-        'js/sticky/jquery.sticky.js',
-        'js/main.js'
-        ])
-    .pipe(concat('app.js'))
+  gulp.src([
+    'js/jquery.js',
+    'js/sticky/jquery.sticky.js',
+    'js/main.js'])
+  .pipe(concat('app.js'))
+  .pipe(gulp.dest('js'));
+});
+
+gulp.task("minifyScripts", function() {
+  gulp.src("js/app.js")
+    .pipe(uglify())
+    .pipe(rename('app.min.js'))
     .pipe(gulp.dest('js'));
 });
 
 gulp.task("default", ["hello"], function() {
-    console.log("the default task!!!!");
+  console.log("This is the default task!");
 });
