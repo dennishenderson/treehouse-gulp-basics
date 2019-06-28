e5eebd4884d6b3a1432c045fbfb3e3e53f806b8c
Dennis Henderson <dennismhenderson@gmail.com>
2019-06-28 04:03:22 -0400

    stage 1 complete

---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+'use strict';
+
+var gulp = require('gulp'),
+  concat = require('gulp-concat');
+
+gulp.task("concatScripts", function() {
+  gulp.src([
+    'js/jquery.js',
+    'js/sticky/jquery.sticky.js',
+    'js/main.js'])
+  .pipe(concat('app.js'))
+  .pipe(gulp.dest('js'));
+});
+
+gulp.task("default", ["hello"], function() {
+  console.log("This is the default task!");
+});
