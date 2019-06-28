0bd0d59a4d30475e2620df26bfe53cac6cdbdfb5
Dennis Henderson <dennismhenderson@gmail.com>
2019-06-28 08:09:14 -0400

    completed gulpfile.js by adding a clean task and watchFiles task to setup a build pipeline

--- gulpfile.js
+++ gulpfile.js
@@ -35,9 +35,10 @@ gulp.task('compileSass', function() {
       .pipe(gulp.dest('css'));
 });
 
-gulp.task('watchSass', function() {
+gulp.task('watchFiles', function() {
   gulp.watch('scss/**/*.scss', ['compileSass']);
-})
+  gulp.watch('js/main.js', ['concatScripts']);
+});
 
 gulp.task('clean', function() {
   del(['dist', 'css/application.css*', 'js/app*.js*']);
@@ -48,5 +49,7 @@ gulp.task("build", ['minifyScripts', 'compileSass'], function() {
                    "img/**", "fonts/**"], { base: './'})
             .pipe(gulp.dest('dist'));
 });
+
+gulp.task('serve', ['watchFiles']);
 
 gulp.task("default", ["build"]);
